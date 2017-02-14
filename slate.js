f193bddf6305a9bb4e41053032c4278b50028c17
Bayless <bsmithsalzberg@stuy.edu>
2017-02-13 19:24:36 -0500

    made rectangles, but they are a little away from the ycor for some reason...

--- slate.js
+++ slate.js
@@ -1,5 +1,10 @@
 var c = document.getElementById("slate");
-c
-var ctx = c.setContext("2d");
+var ctx = c.getContext("2d");
 
 ctx.fillStyle = "ff00ff";
+
+c.addEventListener('click', function(event) {
+    var x = event.clientX;     // Get the horizontal coordinate
+    var y = event.clientY;     // Get the vertical coordinate
+    ctx.fillRect(x,y,Math.random()*100,Math.random()*100);
+});
