0d02bdb53551c04a8c4cde1d1b687d8ddd0bff6f
Karol-Regula <kregula@stuy.edu>
2017-02-13 23:12:55 -0500

    Added clear function, fixed rectangle color, sligthly centered rectangles.

--- slate.js
+++ slate.js
@@ -1,10 +1,16 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 
-ctx.fillStyle = "ff00ff";
+ctx.fillStyle = "#00f0fa";
 
 c.addEventListener('click', function(event) {
-    var x = event.clientX;     // Get the horizontal coordinate
-    var y = event.clientY;     // Get the vertical coordinate
+    var x = event.clientX - 50;     // Get the horizontal coordinate
+    var y = event.clientY - 80;     // Get the vertical coordinate
     ctx.fillRect(x,y,Math.random()*100,Math.random()*100);
+});
+
+var b = document.getElementById("button");
+
+b.addEventListener('click', function(event) {
+    ctx.clearRect(0, 0, 800, 800); //clear entire canvas field
 });
