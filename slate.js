4b1c225ade81376609e2b995f9537ec03832ec99
Karol-Regula <kregula@stuy.edu>
2017-02-15 12:20:27 -0500

    Uncommented rectangle function. Note: code was functional before this commit. (at the required deadline)

--- slate.js
+++ slate.js
@@ -1,30 +1,33 @@
 var c = document.getElementById("slate");
+var b = document.getElementById("button");
 var ctx = c.getContext("2d");
 
 ctx.fillStyle = "#00f0fa";
 
-/*c.addEventListener('click', function(event) {
-    var x = event.clientX - 10;     // Get the horizontal coordinate
-    var y = event.clientY - 60;     // Get the vertical coordinate
+function rect(event) {
+    var x = event.offsetX;     // Get the horizontal coordinate
+    var y = event.offsetY;     // Get the vertical coordinate
     ctx.fillRect(x,y,Math.random()*100,Math.random()*100);
-});
-*/
-var b = document.getElementById("button");
+}
 
-b.addEventListener('click', function(event) {
+function clear(event) {
     ctx.clearRect(0, 0, 800, 800); //clear entire canvas field
     ctx.closePath();
     ctx.beginPath();
-});
+}
 
-
-c.addEventListener('click', function(event) {
-    var x = event.clientX - 10;     // Get the horizontal coordinate
-    var y = event.clientY - 60;     // Get the vertical coordinate
+function circle(event) {
+    //var x = event.clientX - 10;     // Get the horizontal coordinate
+    //var y = event.clientY - 60;     // Get the vertical coordinate
+    var x = event.offsetX;     // Get the horizontal coordinate
+    var y = event.offsetY;     // Get the vertical coordinate
     ctx.lineTo(x,y);
     ctx.moveTo(x+10,y);
     ctx.arc(x,y,10,0,2*Math.PI);
     ctx.moveTo(x,y);
     ctx.fill();
     ctx.stroke();
-});
+}
+
+b.addEventListener('click', clear);
+c.addEventListener('click', circle);
